f8051a747607e90178dd99a55295ff5c2d536fb6
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-26 04:31:22 +0300

    add registration urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url,include
 from . import views
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth.views import LoginView
 urlpatterns = [
     url(r'^$',views.index,name='index'),
+    url(r'^accounts/register/',RegistrationView.as_view(),name="register"),
+    url(r'^accounts/',include('django.contrib.auth.urls')),
+    url(r'^accounts/login/',LoginView, name="login")
 ]
 
