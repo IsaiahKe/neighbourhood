072971e195619f421cfa9c0aec1460b78790c861
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-28 14:43:44 +0300

    update urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url,include
 from . import views
 from django_registration.backends.one_step.views import RegistrationView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     url(r'^$',views.index,name='index'),
-    url(r'^accounts/register/',RegistrationView.as_view(),name="register"),
+    url(r'^accounts/register/',RegistrationView.as_view(success_url='/accounts/login/'),name="register"),
     url(r'^accounts/',include('django.contrib.auth.urls')),
-    url(r'^accounts/login/',LoginView, name="login")
+    url(r'^accounts/login/',LoginView, name="login"),
+    url(r'^addpost/$',views.addpost,name="addpost"),
+    url(r'^profileupdate/(\d)',views.updateprofile,name='updateprofile'),
+    url(r'^business/',views.business,name="business"),
+    url(r'^facility/(\d)/',views.facility,name='facility'),
+    url(r'^search/',views.search,name="serach"),
 ]
 
