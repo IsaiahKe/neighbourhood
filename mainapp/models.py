2806161e7aff6eb668526ce3ffe566fc1c75c17a
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-28 14:43:15 +0300

    update models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,29 +1,36 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserManager
 from cloudinary.models import CloudinaryField
+from django.db.models import fields
+from django.db.models.fields import DateField
 from django.db.models.fields.related import OneToOneField
 from phonenumber_field.modelfields import PhoneNumberField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.utils import timezone
+from .email import send_welcome_email
+from mainapp import email
 # Create your models here.
 class Profile(models.Model):
     photo=CloudinaryField('image')
-    neighbourhood=models.ForeignKey('Neighbourhood',on_delete=models.CASCADE,null=True)
-    contact= PhoneNumberField(blank=True,null=True)
-    user=models.OneToOneField(User,null=True,on_delete=models.CASCADE)
-    admin=models.BooleanField(auto_created=False,null=True)
-    
+    hood=models.ForeignKey('Neighbourhood',on_delete=models.CASCADE,null=True)
+    contact=PhoneNumberField(blank=True)
+    user=models.OneToOneField(User,null=True,on_delete=models.CASCADE,blank=True)
+    admin=models.BooleanField(auto_created=True,null=False,default=False,)
     
     @receiver(post_save,sender=User)
-    def create_profile(sender,instance,created,**kwargs):
+    def create_profile(sender,instance,**kwargs):
         try: 
           instance.profile.save()
         except ObjectDoesNotExist:
             Profile.objects.create(user=instance)
+            #add email function after  registration
     def __str__(self):
-        return f'{User.username} Profile'
+        return f'{self.user}'
+    
+   
+
     
 class Neighbourhood(models.Model):
     name= models.CharField(max_length=30,null=False)
@@ -32,10 +39,50 @@ class Neighbourhood(models.Model):
     
     def __str__(self):
         return self.name
+    def addhood(self):
+        return self.save()
+    def deletehood(self):
+        self.delete()
+    @classmethod
+    def findhood(cls,name):
+        for i in cls:
+            if cls.name==name:
+                return cls
+            return False
 
 
 class Business(models.Model):
     name=models.CharField(null=False,blank=False,max_length=30)
-    category=models.CharField(null=False,max_length=30)
-    residence=models.ForeignKey('Neighbourhood',on_delete=models.CASCADE)    
+    email=models.EmailField(null=True)
+    contact=PhoneNumberField(null=True)
+    location=models.ForeignKey('Neighbourhood',on_delete=models.CASCADE,null=True)
+    
+    def __str__(self):
+        return self.name
+    def addbusiness(self):
+        self.save()
+        
+    def removebusiness(self):
+        self.delete() 
+        
+    
+class Alert(models.Model):
+    content=models.TextField(null=False,blank=True)
+    image=CloudinaryField('image',null=True)
+    author=models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
+    hood=models.ForeignKey('Neighbourhood',null=True,blank=True,on_delete=models.CASCADE)
+    posted=models.DateField(auto_now=timezone.now)
+    
+    
+    def __str__(self):
+        return f'{self.author.username}'
+    
+class Category(models.Model):
+    name=models.CharField(max_length=20,null=False,blank=False)
+    email=models.EmailField(null=True)
+    contact=PhoneNumberField(null=True)
+    location=models.ForeignKey('Neighbourhood',on_delete=models.CASCADE,null=True)
+    def __str__(self):
+        return self.name
+    
     
