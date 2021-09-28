6466d46431bef8811569823dbdf7b3dd2e98d1cc
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-28 14:45:51 +0300

    register models

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Neighbourhood,Profile,Business
+from .models import Alert, Category, Neighbourhood,Profile,Business
 
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(Profile)
 admin.site.register(Business)
+admin.site.register(Alert)
+admin.site.register(Category)
