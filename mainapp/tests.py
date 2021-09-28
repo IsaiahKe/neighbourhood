43832fc15300794f578fa4db71afe267b8375a5f
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-28 14:44:25 +0300

    add test

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.db.models import query
+from django.test import TestCase, testcases
+from .models import Neighbourhood,Alert,Business
 
 # Create your tests here.
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.hood= Neighbourhood(1,'name','location',40)
+        
+    def test_hood(self):
+        self.assertTrue(isinstance(self.hood,Neighbourhood))
+    def test_add(self):
+        self.hood.addhood()
+        query=Neighbourhood.objects.all()
+        self.assertTrue(len(query)>0)
+        
+    def test_delete(self):
+        self.hood.deletehood()
+        query=Neighbourhood.objects.all()
+        self.assertTrue(len(query)==0)
+        
+   
+        
+    
